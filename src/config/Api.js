a4444ad0a27656422e431333167359b4c1d6ec50
Wouter Kurvers <WKurvers11@gmail.com>
2018-10-25 23:55:18 +0200

    init commit

---
+++ src/config/Api.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default class Api {
+    static instance = null;
+
+    url = "http://gaauwe.nl:5000/";
+
+    static getInstance() {
+        if (Api.instance == null) {
+            Api.instance = new Api();
+        }
+
+        return Api.instance;
+    }
+
+    callApi(action, method, data, callBack = response => console.log(response)) {
+
+        if (method == "GET") {
+            fetch(this.url + action, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(responseJson => callBack(responseJson))
+                .catch(error => {
+                    callBack(error);
+                });
+        } else if (method == "POST") {
+            fetch(this.url + action, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(responseJson => callBack(responseJson))
+                .catch(error => {
+                    callBack(error);
+                });
+        }
+
+    }
+}
